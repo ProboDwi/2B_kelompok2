6cee222157df07e5f313e1cf52216c1698e438b5
callherdevia1 <deviakippuw@gmail.com>
2024-11-26 14:05:39 +0700

    update routes

--- routes.php
+++ routes.php
@@ -3,25 +3,25 @@
 
 require_once 'app/controllers/OrdersController.php';
 
-$controller = new OrdersController();
+$orderscontroller = new OrdersController();
 $url = $_SERVER['REQUEST_URI'];
 $requestMethod = $_SERVER['REQUEST_METHOD'];
 
 if ($url == '/orders/index' || $url == '/') {
-    $controller->index();
+    $orderscontroller->index();
 } elseif ($url == '/orders/create' && $requestMethod == 'GET') {
-    $controller->create();
+    $orderscontroller->create();
 } elseif ($url == '/orders/store' && $requestMethod == 'POST') {
-    $controller->store();
+    $orderscontroller->store();
 } elseif (preg_match('/\/orders\/edit\/(\d+)/', $url, $matches) && $requestMethod == 'GET') {
     $userId = $matches[1];
-    $controller->edit($userId);
+    $orderscontroller->edit($userId);
 } elseif (preg_match('/\/orders\/update\/(\d+)/', $url, $matches) && $requestMethod == 'POST') {
     $userId = $matches[1];
-    $controller->update($userId, $_POST);
+    $orderscontroller->update($userId, $_POST);
 } elseif (preg_match('/\/orders\/delete\/(\d+)/', $url, $matches) && $requestMethod == 'GET') {
     $userId = $matches[1];
-    $controller->delete($userId);
+    $orderscontroller->delete($userId);
 } else {
     http_response_code(404);
     echo "404 Not Found";
