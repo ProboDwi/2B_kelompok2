6ea785c8dbfa42ca93a5689598907075399f91ea
callherdevia1 <deviakippuw@gmail.com>
2024-11-27 13:12:57 +0700

    update tampilan crud orders

--- app/views/orders/create.php
+++ app/views/orders/create.php
@@ -2,15 +2,38 @@
 <?php require_once '../public/library/header.php'; ?>
 <?php require_once '../public/library/navbar.php'; ?>
 
-<h2>Tambah Pesanan Baru</h2>
-<form action="/orders/store" method="POST">
-    <label for="tanaman_yang_dipesan">Tanaman Yang Dipesan:</label>
-    <input type="text" name="tanaman_yang_dipesan" id="tanaman_yang_dipesan" required>
-    <label for="pembeli">Pembeli:</label>
-    <input type="text" name="pembeli" id="pembeli" required>
-    <label for="status_pesanan">Status Pesanan:</label>
-    <input type="text" name="status_pesanan" id="status_pesanan" required>
-    <button type="submit">Simpan</button>
-</form>
+<div class="container" style="margin-top: 80px">
+    <div class="row">
+        <div class="col-md-8 offset-md-2">
+            <div class="card">
+                <div class="card-header">Tambah Pesanan Baru</div>
+                    <div class="card-body">
+                        <form action="/orders/store" method="POST">
+
+                            <div class="form-group">
+                                <label for="tanaman_yang_dipesan">Tanaman Yang Dipesan</label>
+                                <input type="text" name="tanaman_yang_dipesan" id="tanaman_yang_dipesan" class="form-control" required>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="pembeli">Pembeli</label>
+                                <input type="text" name="pembeli" id="pembeli" class="form-control" required>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="status_pesanan">Status Pesanan</label>
+                                <input type="text" name="status_pesanan" id="status_pesanan" class="form-control" required>
+                            </div>
+
+                            <button type="submit" class="btn btn-success">SIMPAN</button>
+                            <button type="reset" class="btn btn-warning">RESET</button>
+                            <a class="btn btn-primary" href="/orders/index" role="button">KEMBALI</a>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 <?php require_once '../public/library/footer.php'; ?>
